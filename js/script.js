2a77ac5a13b4e9bcf8473118c5aa77520fb6d13d
Norbert Mielczarek <nri1234.nm@gmail.com>
2019-05-26 20:52:15 +0200

    Add modals and new game params to script.js

--- js/script.js
+++ js/script.js
@@ -1,63 +1,49 @@
 'use strict';
-var roundsNumber;
-var roundsPlayed = 0;
-var gameActive = false;
-var playerName = window.prompt('What is your name ?');
-var name = playerName;
+var params = {
+    roundsNumber: 0,
+    roundsPlayed: 1,
+    gameActive: false,
+    compWins: 0,
+    playerWins: 0,
+    progress: [],
+    playerName: "Player"
+};
 var output = document.getElementById("output");
-var player = document.getElementById("player");
-player.innerHTML = "<strong>" + name;
-var newGame = document.getElementById("newGame");
-var rounds = document.getElementById("rounds");
-// Buttons to choose move by player//
-var playerMoveRock = document.getElementById("playerMove_rock"),
-    playerMovePaper = document.getElementById("playerMove_paper"),
-    playerMoveScissors = document.getElementById("playerMove_scissors");
 
-// player choice function by click//
-playerMoveRock.addEventListener("click", function () {
-    if (gameActive) {
-        playerMove("rock");
-    } else {
-        inactiveGame();
-    }
-});
-playerMovePaper.addEventListener("click", function () {
-    if (gameActive) {
-        playerMove("paper");
-    } else {
-        inactiveGame();
-    }
-});
-playerMoveScissors.addEventListener("click", function () {
-    if (gameActive) {
-        playerMove("scissors");
-    } else {
-        inactiveGame();
-    }
-});
-//displays information text about the game//
 function displayText(text) {
     output.innerHTML = text + "<br>";
 }
 
 function inactiveGame() {
-    displayText("Game Over!!! <br> Please press the Start new game button!");
+    displayText(" Please press the Start new game button!");
 }
-
-// displays game result//
-var compWins = 0,
-    playerWins = 0,
-    result = document.getElementById("result");
-var displayResult = function (playerWins, compWins) {
+// providing game's result
+var result = document.getElementById("result");
+var displayResult = function () {
     result.innerHTML =
-        "<strong>" + //makes words bold//
-        playerWins +
+        "<p> <strong>" +
+        params.playerWins +
         "</strong> - <strong>" +
-        compWins +
-        "</strong>";
+        params.compWins +
+        "</strong></p>";
 };
-displayResult(playerWins, compWins);
+displayResult();
+
+
+
+// player choice function by click//
+var playerMoves = document.querySelectorAll(".player-move");
+for (var i = 0; i < playerMoves.length; i++) {
+    playerMoves[i].addEventListener("click", function () {
+        var playersChoice = this.getAttribute("data-move");
+        if (params.gameActive) {
+            // invoking playerMove function with a payer's choice parameter
+            playerMove(playersChoice);
+        } else {
+            inactiveGame();
+        }
+    });
+}
 
 // Computer move function//
 function compMove() {
@@ -72,95 +58,185 @@ function compMove() {
     }
 }
 
-// determining who won the round//
+// determining who won the round
 function win(playerChoice, compChoice) {
-    if (playerChoice === compChoice) { //draw//
-        return "Ups, nobody ";
+    if (playerChoice === compChoice) {
+        return "Nobody ";
     } else if (
         (playerChoice === "paper" && compChoice === "rock") ||
         (playerChoice === "rock" && compChoice === "scissors") ||
         (playerChoice === "scissors" && compChoice === "paper")
     ) {
-        playerWins++;
-        return name;
+        params.playerWins++;
+        return params.playerName;
     } else {
-        compWins++;
+        params.compWins++;
         return "Computer";
     }
+}
+//
+function createProgressTable() {
+    var progressTableContent = "";
+    for (var i = 0; i < params.progress.length; i++) {
+        progressTableContent +=
+            "<tr><td>" +
+            params.progress[i].round +
+            "</td><td>" +
+            params.progress[i].winner +
+            "</td><td>" +
+            params.progress[i].playerMove +
+            "</td><td>" +
+            params.progress[i].compMove +
+            "</td><td>" +
+            params.progress[i].roundResult +
+            "</td></tr>";
+    }
+
+    var progressTableHeading =
+        "<table><tr><th>Games</th><th>Winner</th><th>Player</th><th>Computer</th><th>Result</th></tr>";
+
+    var progressTable = progressTableHeading + progressTableContent + "</table>";
+    return progressTable;
 }
 // displays text of the results of the game, who won each game//
 function playerMove(playerChoice) {
     var compChoice = compMove();
-    var gameResult = win(playerChoice, compChoice);
-    displayResult(playerWins, compWins);
-    roundsPlayed++;
 
-    if (playerWins === roundsNumber) {
-        displayText(
-            "<srtong>" +
-            name +
-            "," +
-            " You won the entire game!!! Round " +
-            roundsPlayed +
-            ": <strong>" +
-            gameResult +
-            "</strong> won. played <strong>" +
-            playerChoice +
-            "</strong>, computer played <strong>" +
-            compChoice +
-            "</strong> "
-        );
-        gameActive = false; //stops carry on playing after number of wins, START NEW GAME!!//
-    } else if (compWins === roundsNumber) {
-        displayText(
-            " Computer won the entire game!!! Round " +
-            roundsPlayed +
-            ": <strong>" +
-            gameResult +
-            "</strong> won.You played <strong>" +
-            playerChoice +
-            "</strong>, computer played <strong>" +
-            compChoice +
-            "</strong> "
-        );
-        gameActive = false;
+    var gameResult = win(playerChoice, compChoice);
+    displayResult();
+    params.roundsPlayed++;
+
+    var roundProgress = {
+        round: params.roundsPlayed,
+        winner: gameResult,
+        playerMove: playerChoice,
+        compMove: compChoice,
+        roundResult: params.playerWins + " : " + params.compWins
+    };
+    params.progress.push(roundProgress);
+
+    var resultText =
+        " Round " +
+        params.roundsPlayed +
+        ": <strong>" +
+        gameResult +
+        "</strong> won." +
+        params.playerName +
+        " played <strong>" +
+        playerChoice +
+        "</strong>, computer played <strong>" +
+        compChoice +
+        "</strong> ";
+    if (
+        params.playerWins === params.roundsNumber ||
+        params.compWins === params.roundsNumber
+    ) {
+        var progressTable = createProgressTable();
+        var winner = gameResult.toUpperCase();
+        var modalContent =
+            "<p>" + winner + " won the entire game!!!</p>" + progressTable;
+        var modalToBeDisplayed = "#modal-game-over";
+        addModalContent(modalToBeDisplayed, modalContent);
+        showModal(modalToBeDisplayed);
+        params.gameActive = false;
+        displayText(resultText);
     } else {
-        displayText(
-            "Round " +
-            roundsPlayed +
-            ": <strong>" +
-            gameResult +
-            "</strong> won. You played <strong>" +
-            playerChoice +
-            "</strong>, computer played <strong>" +
-            compChoice +
-            "</strong>"
-        );
+        displayText(resultText);
     }
 }
-//function with starting new game//
+
+
+
+//function starting new game//
 newGame.addEventListener("click", function () {
-    playerWins = 0;
-    compWins = 0;
-    roundsPlayed = 0;
-    displayResult(playerWins, compWins);
+    params.playerWins = 0;
+    params.compWins = 0;
+    params.roundsPlayed = 0;
+    params.progress = [];
+    displayResult();
     displayText(
         "Let's see who's going to win."
     );
-    //what if player will choose not a number//
-    var userInput = window.prompt("How many rounds do you want to play?");
+    showModal("#modal-new-game"); //shows modal with no of wins and player's name//
+});
 
-    if (userInput === null || userInput === "") {
-        rounds.innerHTML = "You didn't provide any value";
-    } else if (isNaN(userInput)) {
+// getting new game params
+var submitNewGame = document.getElementById("submit-new-game");
+
+submitNewGame.addEventListener("click", function () {
+    event.preventDefault();
+    hideModal(event);
+    //get player name//
+    var playerInputName = document.getElementById("inputName").value;
+    var player = document.getElementById("player");
+    player.innerHTML = "<strong>" + playerInputName;
+    if (playerInputName === null || playerInputName === "") {
+        params.playerName = "Player";
+    } else {
+        params.playerName = playerInputName;
+        displayResult();
+    }
+
+    // get wins number //
+    var roundsInput = document.getElementById("inputRounds").value;
+    if (roundsInput === null || roundsInput === "") {
+        rounds.innerHTML = "You didn't provide a value";
+    } else if (isNaN(roundsInput)) {
         rounds.innerHTML = "The value you provided is not a number";
-    } else if (parseInt(userInput) <= 0) {
+    } else if (parseInt(roundsInput) <= 0) {
         rounds.innerHTML =
             "The value you provided is not a possible number of rounds";
     } else {
-        gameActive = true;
-        roundsNumber = parseInt(userInput);
+        params.gameActive = true;
+        params.roundsNumber = parseInt(roundsInput);
+
         rounds.innerHTML =
-            "The game will end after <strong>" + roundsNumber + "</strong> wins";
+            "The game will end after <strong>" +
+            params.roundsNumber +
+            "</strong> wins";
     }
 });
+
+// remove "show" class from all modals //
+function hideModals() {
+    for (var i = 0; i < modals.length; i++) {
+        modals[i].classList.remove("show");
+    }
+}
+
+// function adding modal content //
+var addModalContent = function (modalsID, modalContent) {
+    var modalChanged = document.querySelector(modalsID);
+    modalChanged.querySelector(".content").innerHTML = modalContent;
+};
+
+// function to open modal //
+var showModal = function (modalShown) {
+    hideModals();
+    document.querySelector(modalShown).classList.add("show");
+    document.querySelector("#modal-overlay").classList.add("show");
+};
+
+//close modal//
+var hideModal = function (event) {
+    event.preventDefault();
+    document.querySelector("#modal-overlay").classList.remove("show");
+};
+
+var closeButtons = document.querySelectorAll(".modal .close");
+
+for (var i = 0; i < closeButtons.length; i++) {
+    closeButtons[i].addEventListener("click", hideModal);
+}
+
+// closing by clicking overlay //
+document.querySelector("#modal-overlay").addEventListener("click", hideModal);
+
+// stop propagation in overlay //
+var modals = document.querySelectorAll(".modal");
+
+for (var i = 0; i < modals.length; i++) {
+    modals[i].addEventListener("click", function (event) {
+        event.stopPropagation();
+    });
+}
