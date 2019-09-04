31e7d22718407d5eafa3718c3e1774ab3e86235c
Norbert Mielczarek <nri1234.nm@gmail.com>
2019-09-04 08:59:46 +0200

    Made changes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 var roundsNumber;
 var roundsPlayed = 0;
 var gameActive = false;
-var playerName = window.prompt('What is your name ?');
+var playerName = window.prompt("What is your name ?");
 var name = playerName;
 var output = document.getElementById("output");
 var player = document.getElementById("player");
@@ -15,21 +15,21 @@ var playerMoveRock = document.getElementById("playerMove_rock"),
     playerMoveScissors = document.getElementById("playerMove_scissors");
 
 // player choice function by click//
-playerMoveRock.addEventListener("click", function () {
+playerMoveRock.addEventListener("click", function() {
     if (gameActive) {
         playerMove("rock");
     } else {
         inactiveGame();
     }
 });
-playerMovePaper.addEventListener("click", function () {
+playerMovePaper.addEventListener("click", function() {
     if (gameActive) {
         playerMove("paper");
     } else {
         inactiveGame();
     }
 });
-playerMoveScissors.addEventListener("click", function () {
+playerMoveScissors.addEventListener("click", function() {
     if (gameActive) {
         playerMove("scissors");
     } else {
@@ -49,7 +49,7 @@ function inactiveGame() {
 var compWins = 0,
     playerWins = 0,
     result = document.getElementById("result");
-var displayResult = function (playerWins, compWins) {
+var displayResult = function(playerWins, compWins) {
     result.innerHTML =
         "<strong>" + //makes words bold//
         playerWins +
@@ -74,7 +74,8 @@ function compMove() {
 
 // determining who won the round//
 function win(playerChoice, compChoice) {
-    if (playerChoice === compChoice) { //draw//
+    if (playerChoice === compChoice) {
+        //draw//
         return "Ups, nobody ";
     } else if (
         (playerChoice === "paper" && compChoice === "rock") ||
@@ -98,55 +99,53 @@ function playerMove(playerChoice) {
     if (playerWins === roundsNumber) {
         displayText(
             "<srtong>" +
-            name +
-            "," +
-            " You won the entire game!!! Round " +
-            roundsPlayed +
-            ": <strong>" +
-            gameResult +
-            "</strong> won. played <strong>" +
-            playerChoice +
-            "</strong>, computer played <strong>" +
-            compChoice +
-            "</strong> "
+                name +
+                "," +
+                " You won the entire game!!! Round " +
+                roundsPlayed +
+                ": <strong>" +
+                gameResult +
+                "</strong> won. played <strong>" +
+                playerChoice +
+                "</strong>, computer played <strong>" +
+                compChoice +
+                "</strong> "
         );
         gameActive = false; //stops carry on playing after number of wins, START NEW GAME!!//
     } else if (compWins === roundsNumber) {
         displayText(
             " Computer won the entire game!!! Round " +
-            roundsPlayed +
-            ": <strong>" +
-            gameResult +
-            "</strong> won.You played <strong>" +
-            playerChoice +
-            "</strong>, computer played <strong>" +
-            compChoice +
-            "</strong> "
+                roundsPlayed +
+                ": <strong>" +
+                gameResult +
+                "</strong> won.You played <strong>" +
+                playerChoice +
+                "</strong>, computer played <strong>" +
+                compChoice +
+                "</strong> "
         );
         gameActive = false;
     } else {
         displayText(
             "Round " +
-            roundsPlayed +
-            ": <strong>" +
-            gameResult +
-            "</strong> won. You played <strong>" +
-            playerChoice +
-            "</strong>, computer played <strong>" +
-            compChoice +
-            "</strong>"
+                roundsPlayed +
+                ": <strong>" +
+                gameResult +
+                "</strong> won. You played <strong>" +
+                playerChoice +
+                "</strong>, computer played <strong>" +
+                compChoice +
+                "</strong>"
         );
     }
 }
 //function with starting new game//
-newGame.addEventListener("click", function () {
+newGame.addEventListener("click", function() {
     playerWins = 0;
     compWins = 0;
     roundsPlayed = 0;
     displayResult(playerWins, compWins);
-    displayText(
-        "Let's see who's going to win."
-    );
+    displayText("Let's see who's going to win.");
     //what if player will choose not a number//
     var userInput = window.prompt("How many rounds do you want to play?");
 
@@ -161,6 +160,8 @@ newGame.addEventListener("click", function () {
         gameActive = true;
         roundsNumber = parseInt(userInput);
         rounds.innerHTML =
-            "The game will end after <strong>" + roundsNumber + "</strong> wins";
+            "The game will end after <strong>" +
+            roundsNumber +
+            "</strong> wins";
     }
 });
